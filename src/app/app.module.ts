aca905f58ccdb52b6f3caffab15cc0bb1c18d5df
Javier <jbenito.dz@gmail.com>
2018-10-17 18:24:00 +0200

    Added shell module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ShellModule } from 'src/app/shell/shell.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShellModule
   ],
   providers: [],
   bootstrap: [
