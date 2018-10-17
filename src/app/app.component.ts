d369c581bd539d3a5d8cf89ed1db065cfb574f8d
Javier <jbenito.dz@gmail.com>
2018-10-17 15:22:29 +0200

    Added loading for loading lazy modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-realworld-base';
+export class AppComponent implements OnInit  {
+  loadingLazyModule: boolean;
+
+  constructor (private router: Router) {}
+
+  ngOnInit () {
+    this.router.events.subscribe(event => {
+        if (event instanceof RouteConfigLoadStart) {
+            this.loadingLazyModule = true;
+        } else if (event instanceof RouteConfigLoadEnd) {
+            this.loadingLazyModule = false;
+        }
+    });
+  }
 }
