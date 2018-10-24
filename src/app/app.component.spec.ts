4c54908cb35713241062ed3da1ac8984cc496eab
Javier <jbenito.dz@gmail.com>
2018-10-24 11:53:42 +0200

    updated to Angular 7

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,20 +2,16 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { appRoutes } from './app-routing.module';
-import { ShellComponent } from 'src/app/shell/shell.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule.withRoutes(appRoutes),
-        // BrowserAnimationsModule
       ],
       declarations: [
-        AppComponent,
-        // ShellComponent
+        AppComponent
       ],
       schemas: [
         CUSTOM_ELEMENTS_SCHEMA
